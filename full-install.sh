fc528f2278ebe4f6d5650c2b9850b62f5c3c42fc
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-04 14:18:45 +0100

    Update full-install.sh

--- full-install.sh
+++ full-install.sh
@@ -1,23 +1,23 @@
 if [ -d "$HOME/nano-highlight" ]; then
   echo "nano-highlight folder exists, skipping folder setup..."
-  cd $HOME/nano-highlight
 else
   echo -n "Setting up highlighter folder..."
   mkdir $HOME/nano-highlight
   cd $HOME/nano-highlight
+  touch .Nano-highlight-exists
   wget https://raw.githubusercontent.com/TylerMS887/nano-highlight/main/README-installed-full -O README -q
   echo " Done!"
 fi
-if [ -f "$HOME/.nanorc" ] then
-  echo "Skipping nanorc setup"
-else
-  echo "Installing highlighters..."
-  echo 'include "~/nano-highlight/.*.nanorc"' >> $HOME/.nanorc
-fi
-echo "Downloading highlighters..."
+echo "Downloading highlighters [latest version]..."
 echo -n "- Dart"
 wget https://raw.githubusercontent.com/TylerMS887/nano-highlight/main/.dart.nanorc -q
 echo " ✓"
+if [ [ ! -z $(grep "include \"$HOME/nano-highlight/.*.nanorc\"" "$HOME/.nanorc") ] ] then
+  echo "nano-highlight loader is in the nanorc file, leaving nanorc alone..."
+else
+  echo "Enabling highlighters..."
+  echo "include \"$HOME/nano-highlight/.*.nanorc\"" >> $HOME/.nanorc
+fi
 if [ -f "$HOME/nano-highlight/README" ]
 then
 cat README
