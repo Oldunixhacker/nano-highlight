1e6d9665dd5c7798f58934d997070f7c325b2ed3
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-04 14:06:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
-echo -n "Setting up highlighter folder..."
-mkdir ~/nano-highlight
-cd ~/nano-highlight
-wget https://raw.githubusercontent.com/TylerMS887/nano-highlight/main/README-installed -O README -q
-echo " Done!"
-echo "Enabling highlighters..."
-echo 'include "~/nano-highlight/.*.nanorc"' >> ~/.nanorc
-cat README
-rm README
+if [ -d "~/nano-highlight" ]; then
+  echo "nano-highlight folder exists, exiting setup..."
+else
+  echo -n "Setting up highlighter folder..."
+  mkdir ~/nano-highlight
+  cd ~/nano-highlight
+  wget https://raw.githubusercontent.com/TylerMS887/nano-highlight/main/README-installed-full -O README -q
+  echo " Done!"
+  echo "Enabling highlighters..."
+  echo 'include "~/nano-highlight/.*.nanorc"' >> ~/.nanorc
+  cat README
+  rm README
+fi
