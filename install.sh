6ccd33eacf961e2cb5c7fa09574d1231f7c8a25e
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-04 13:54:49 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo -n "Setting up highlighter folder..."
+mkdir ~/nano-highlight
+cd ~/nano-highlight
+wget https://raw.githubusercontent.com/TylerMS887/nano-highlight/main/README-installed -O README -q
+echo " Done!"
+echo "Enabling highlighters..."
+echo 'include "~/nano-highlight/.*.nanorc"' >> ~/.nanorc
+cat README
+rm README
